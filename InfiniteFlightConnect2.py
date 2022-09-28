ee0735de64566c9eb9b83f699863ea0bfcfc7655
David Wu <toffino@aliyun.com>
2022-09-28 14:06:16 -0400

    tried the util function

--- InfiniteFlightConnect2.py
+++ InfiniteFlightConnect2.py
@@ -1,8 +1,11 @@
+from importlib.util import spec_from_file_location
+from os import uname
 import time
 import struct
 import socket
 import json
 import logging
+from util import recieve, unpack
 
 logger = logging.getLogger()
 ch = logging.StreamHandler()
@@ -11,7 +14,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 logger.addHandler(fh)
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.DEBUG)
 
 class IFClient(object):
     def __init__(self) -> None:
@@ -79,26 +82,21 @@ class IFClient(object):
 
         #logger.debug('{}'.format(entries))
     
-    def get_state(self, id):
+    def get_state(self, id, data_type):
         request = struct.pack('<lx', id)
-        logger.debug('{}'.format(request))
         self.conn.sendall(request)
-        response = self.conn.recv(4)
-        logger.debug('{}'.format(response))
-        return_id = struct.unpack('<l', response)[0]
-        response = self.conn.recv(4)
-        return_length = struct.unpack('<l', response)[0]
-        recved = 0
-        response = bytes()
-        while recved < return_length:
-            response += self.conn.recv(return_length - recved)
-            recved = len(response)
-        
-        pay_load = response[4:]
-        lat = struct.unpack('<f', pay_load)[0]
-        logger.debug('{}'.format(lat))
+        response = recieve(self.conn, 4)
+        return_id = unpack(response, data_type=1)
+        response = recieve(self.conn, 4)
+        return_length = unpack(response, data_type=1)
+        logging.debug('return: id: {}, length: {}'.format(return_id, return_length))
+        pay_load = recieve(self.conn, return_length)
 
-        return lat
+        return_value = unpack(pay_load, data_type)
+
+        logger.debug('{}'.format(return_value))
+
+        return return_value
 
 
     
@@ -109,10 +107,16 @@ class IFClient(object):
 
 if __name__ == '__main__':
     ifc = IFClient()
-    ifc.get_manifest()
+    #ifc.get_manifest()
     for i in range(500):
-        lat = ifc.get_state(628)
-        print('current lat: {}'.format(lat))
+        lat = ifc.get_state(624, 3)
+        lng = ifc.get_state(625, 3)
+        isbooted = ifc.get_state(944, 0)
+        livery = ifc.get_state(538, 4)
+        oiltemp = ifc.get_state(368, 2)
+
+
+        print('{}: isbooted: {}, current lat: {}, {}, oil temp: {}'.format(livery, isbooted, lat, lng, oiltemp))
         time.sleep(10)
 
     ifc.close()
