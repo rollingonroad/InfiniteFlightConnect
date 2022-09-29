8bd8a5aceb8562c1560911f62d36c8fa625b6535
David Wu <toffino@aliyun.com>
2022-09-28 22:52:54 -0400

    add pack function

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+from inspect import istraceback
 import struct
 # convert byte array to specific DataType, raise exception if payload length mismatch with DataType
 def unpack(payload, data_type):
@@ -21,6 +22,23 @@ def unpack(payload, data_type):
         pass
 
     return return_value
+
+def pack(value, data_type):
+    if data_type == 0 and isinstance(value, bool):
+        return struct.pack('?', value)
+    if data_type == 1 and isinstance(value, int):
+        return struct.pack('<l', value)
+    if data_type == 2 and isinstance(value, float):
+        return struct.pack('<f', value)
+    if data_type == 3 and isinstance(value, float):
+        return struct.pack('<d', value)
+    if data_type == 4 and isinstance(value, str):
+        return struct.pack('<l', len(value)) + value.encode('utf-8')
+    if data_type == 5 and isinstance(value, int):
+        return struct.pack('<q', value)
+    else:
+        # todo: raise exception
+        pass
 
 # recieve specific size from the socket
 def recieve(conn, size):
