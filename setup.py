95a5eb03e8bf93f635858d1b717bf62df3183011
David Wu <toffino@aliyun.com>
2022-09-30 00:29:19 -0400

    add some information to setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,14 @@ setuptools.setup(
     description="Using python to connect the Infinite Flight Connect API v2.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/rollingonroad/InfiniteFlightConnect",
     include_package_data=False,
     package_data={},
     packages=setuptools.find_packages(),
-    classifiers=[],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
 )
